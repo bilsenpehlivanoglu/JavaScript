507ec18bcc420eb3afd3900716e12ceeea0e819b
Bilsen Pehlivanoglu <bilsensaturnus@gmail.com>
2024-08-23 19:50:31 -0400

    first commit

---
+++ Variables.js
@@ -0,0 +1,23 @@
+var myName;
+console.log(myName);//by default,cariables are undefined
+
+//var variables can be used multiple times
+var myName='JavaScript';
+console.log(myName);//"JavaScript"
+
+var myName='Java';
+console.log(myName);//"Java"
+
+myName=true;
+console.log(myName);//true
+
+myName=9;
+console.log(myName);//9
+
+var firstName='Neo', lastName='Trinity';
+console.log(firstName);//"Neo"
+console.log(lastName);//"Trinity"
+
+var fName='Bilsen',
+    lName='Pehlivanoglu'
+    console.log(fName+ ' '+lName);//Bilsen Pehlivanoglu
