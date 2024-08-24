7298c1d0c39a526ed87d728db1f0c9e7d4e34586
Bilsen Pehlivanoglu <bilsensaturnus@gmail.com>
2024-08-24 09:03:47 -0400

    JS

---
+++ FunctionalVsScopedBlocks.js
@@ -0,0 +1,13 @@
+
+//Function scoped variable available anywhere inside the function that is declared in
+
+//Block scope function is only available only inside of the block such as if or loop
+
+// We need these terms to understand vale,const
+
+//SUCH AS
+var a = "some value"; //functional or global scoped
+
+let b = "some value"; //block scoped
+
+const c = "some value"; // block scoped + cannor get new value
---
+++ Important_Information_About.js
@@ -0,0 +1,25 @@
+
+//NAMING CONVENTIONS FOR JS VARIABLES
+// 1) Give meaningful names -> firstName for first name
+// 2) Can not use reserved keywords like var,let,const,alret,etc...
+// 3) Can not start with numbers
+// 4) Can not have space
+// 5) Use camelCase
+// Note:variable names are case sensitive firstName;FirstName; are different 
+// Use single or double quote for String data type ->such as 'Hello'
+
+// JavaScript is dynamic language.It means the type of the variable can change at runtime 
+// Unlike static language,the type of the variable are determined base on the type of assign value
+ 
+// Static(statically-typed) --> string name = 'Neo'
+// Dynamic(dynamically-typed) --> let name = 'Neo'
+
+//DEFINING VARIABLES
+// var,const,let
+// let and const, ES6 came with ES6 (Modern JavaScript)
+
+// Such as --> const pi=3.14 (do not change never)
+// var counter = 1; --> may change
+// let ager = 30; -->may change ( var and let similar)
+
+// ; --> is not mandatory
